10b1683db9e8a7f8ccc6cfcc720e3cf055d3c74d
Tonghie R. Sy Jr. <trsyjr@ENTDSWD.LOCAL>
2026-02-26 16:02:33 +0800

    Added LAzy Loading for Optimizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
 // src/App.jsx
+import React, { Suspense, lazy, useState, useEffect } from "react"; // Added Suspense and lazy
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import HomePage from "./components/HomePage";
-import OtherOptions from "./components/OtherOptions";
-import NewsEvents from "./components/NewsEvents";
-import About from "./pages/About";
-import KnowledgeBank from "./pages/KnowledgeBank";
-import Resources from "./pages/Resources";
-import ActiveProfile from "./pages/ActiveProfile";
-import Ldi from "./pages/Ldi";
-import Participant from "./pages/Participant";
-import TASupport from "./pages/TASupport";
-import LD from "./pages/LD";
-import CBA from "./pages/CBA";
-import CBPlan from "./pages/CBPlan";
 
-// New import for TrainingCalendar
-import TrainingCalendar from "./components/TrainingCalendar";
-
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import TermsAndConditions from "./pages/TermsAndCondition";
+// Pages/Components converted to Lazy Imports
+const HomePage = lazy(() => import("./components/HomePage"));
+const OtherOptions = lazy(() => import("./components/OtherOptions"));
+const NewsEvents = lazy(() => import("./components/NewsEvents"));
+const About = lazy(() => import("./pages/About"));
+const KnowledgeBank = lazy(() => import("./pages/KnowledgeBank"));
+const Resources = lazy(() => import("./pages/Resources"));
+const ActiveProfile = lazy(() => import("./pages/ActiveProfile"));
+const Ldi = lazy(() => import("./pages/Ldi"));
+const Participant = lazy(() => import("./pages/Participant"));
+const TASupport = lazy(() => import("./pages/TASupport"));
+const LD = lazy(() => import("./pages/LD"));
+const CBA = lazy(() => import("./pages/CBA"));
+const CBPlan = lazy(() => import("./pages/CBPlan"));
+const TrainingCalendar = lazy(() => import("./components/TrainingCalendar"));
+const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
+const TermsAndConditions = lazy(() => import("./pages/TermsAndCondition"));
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useState, useEffect } from "react";
 import Preloader from "./components/Preloader";
 
 function AppContent() {
@@ -51,41 +50,43 @@ function AppContent() {
     return () => clearTimeout(minTimeout);
   }, [location.pathname]);
 
-  if (isLoading) return <Preloader />;
-
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <HomePage />
-              <OtherOptions />
-              <NewsEvents />
-            </>
-          }
-        />
-        <Route path="/about" element={<About />} />
-        <Route path="/knowledgebank" element={<KnowledgeBank />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/active-profile" element={<ActiveProfile />} />
-        <Route path="/ldi-dip" element={<Ldi />} />
-        <Route path="/participant-eligibility" element={<Participant />} />
-        <Route path="/ta-support" element={<TASupport />} />
-        <Route path="/ld-standards" element={<LD />} />
-        <Route path="/cbas" element={<CBA />} />
-        <Route path="/cbplan" element={<CBPlan />} />
-
-        {/* New Training Calendar Route */}
-        <Route path="/training-calendar" element={<TrainingCalendar />} />
-
-        {/* New full pages */}
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-      </Routes>
-      <Footer />
+      <Suspense fallback={<Preloader />}>
+        {isLoading ? (
+          <Preloader />
+        ) : (
+          <>
+            <Navbar />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <HomePage />
+                    <OtherOptions />
+                    <NewsEvents />
+                  </>
+                }
+              />
+              <Route path="/about" element={<About />} />
+              <Route path="/knowledgebank" element={<KnowledgeBank />} />
+              <Route path="/resources" element={<Resources />} />
+              <Route path="/active-profile" element={<ActiveProfile />} />
+              <Route path="/ldi-dip" element={<Ldi />} />
+              <Route path="/participant-eligibility" element={<Participant />} />
+              <Route path="/ta-support" element={<TASupport />} />
+              <Route path="/ld-standards" element={<LD />} />
+              <Route path="/cbas" element={<CBA />} />
+              <Route path="/cbplan" element={<CBPlan />} />
+              <Route path="/training-calendar" element={<TrainingCalendar />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+            </Routes>
+            <Footer />
+          </>
+        )}
+      </Suspense>
       <ToastContainer position="top-right" autoClose={3000} />
     </>
   );
